bd439e06e822b16590cdbee3002555098983c5a4
Colin Barrett <colin@springsandstruts.com>
2011-08-15 20:20:57 -0700

    First working version of the Haskell calculator.

--- xpc-calc-service-hs/HsXPC.hs
+++ xpc-calc-service-hs/HsXPC.hs
@@ -15,7 +15,7 @@ type XPCConnection = Ptr XPC
 type XPCObject = Ptr XPC
 type XPCType = Ptr XPC
 
---foreign export ccall hsEventHandler :: XPCConnection -> XPCObject -> IO ()
+foreign export ccall hsEventHandler :: XPCConnection -> XPCObject -> IO ()
 
 foreign import ccall unsafe "xpc/xpc.h xpc_get_type"
   xpc_get_type :: XPCObject -> XPCType
@@ -30,16 +30,16 @@ foreign import ccall unsafe "xpc/xpc.h xpc_array_get_count"
   xpc_array_get_count :: XPCObject -> CSize
 
 foreign import ccall unsafe "xpc/xpc.h xpc_array_get_int64"
-  xpc_array_get_int64 ::  XPCObject -> CSize -> CInt
+  xpc_array_get_int64 ::  XPCObject -> CSize -> CLLong
 
 foreign import ccall unsafe "xpc/xpc.h xpc_array_set_int64"
-  xpc_array_set_int64 :: XPCObject -> CSize -> CInt -> IO ()
+  xpc_array_set_int64 :: XPCObject -> CSize -> CLLong -> IO ()
 
 foreign import ccall unsafe "xpc/xpc.h xpc_dictionary_create_reply"
   xpc_dictionary_create_reply :: XPCObject -> IO XPCObject
 
 foreign import ccall unsafe "xpc/xpc.h xpc_dictionary_get_int64"
-  xpc_dictionary_get_int64 :: XPCObject -> CString -> CInt
+  xpc_dictionary_get_int64 :: XPCObject -> CString -> CLLong
 
 foreign import ccall unsafe "xpc/xpc.h xpc_dictionary_get_value"
   xpc_dictionary_get_value :: XPCObject -> CString -> XPCObject
@@ -53,7 +53,7 @@ foreign import ccall unsafe "xpc/connection.h xpc_connection_send_message"
 foreign import ccall unsafe "xpc/xpc.h &xpc_release"
   finalizerXPCRelease :: FunPtr (XPCObject -> IO ())
 
-xpcArrayToList xa = fromIntegral . xpc_array_get_int64 xa <$> [0 .. xpc_array_get_count xa]
+xpcArrayToList xa = fromIntegral . xpc_array_get_int64 xa <$> [0 .. xpc_array_get_count xa - 1]
 
 withNewXPCPtr xpcObjIO f = xpcObjIO >>= newForeignPtr finalizerXPCRelease >>= (flip withForeignPtr) f
 
@@ -71,11 +71,25 @@ sendReply peer event f = do
   withCString "stack" $ \stackStr -> do
   withXPCArray (f op stack) $ \newStack -> do
     xpc_dictionary_set_value reply stackStr newStack
-    xpc_connection_send_message peer reply        
+    xpc_connection_send_message peer reply
 
+consumeBinary :: (Int -> Int -> Int) -> [Int] -> [Int]
+consumeBinary f xs
+  | len >= 2  = xs' ++ [lhs `f` rhs]
+  | otherwise = xs
+  where (xs', rhs:lhs:_) = splitAt (len - 2) xs
+        len = length xs
+
+-- TODO ops should be bound to the values in the enum in Shared.h
+calc :: Int -> [Int] -> [Int]
+calc 0 = consumeBinary (+)
+calc 1 = consumeBinary (-)
+calc 2 = consumeBinary (*)
+calc 3 = consumeBinary div
+  
 hsEventHandler :: XPCConnection -> XPCObject -> IO ()
 hsEventHandler peer event = 
   if eventType == xpc_type_error then return ()
-  else do 
-    sendReply peer event $ \op xs -> [1..5]
+  else do
+    sendReply peer event calc
   where eventType = xpc_get_type event
